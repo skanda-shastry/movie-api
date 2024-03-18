4a6c7857f771bebf89e23b17d379d3896961b911
Skanda shastry <skanda.shastry02@gmail.com>
2024-03-18 10:39:25 +0530

    k8s manifest

--- app/services.py
+++ app/services.py
@@ -1,22 +1,31 @@
 from app.utils import get_db
+import json
 
 def get_movies_by_year(year):
     db = get_db()
-    movies = list(db.movies.find({"year": year}))
+    #print(type(year))
+    year_in_int = int(year)
+    movies = list(db.movies.find({"year": year_in_int}))
+
+    #print(list(movies))
+    movies = json.dumps(movies, default=str)
 
     return movies
 
 def get_movies_by_title(title):
     db = get_db()
     movies = list(db.movies.find({"title": title}))
+    movies = json.dumps(movies, default=str)
     return movies
 
 def get_movies_by_cast(cast):
     db = get_db()
     movies = list(db.movies.find({"cast": cast}))
+    movies = json.dumps(movies, default=str)
     return movies
 
 def get_movies_by_genre(genre):
     db = get_db()
     movies = list(db.movies.find({"genre": genre}))
+    movies = json.dumps(movies, default=str)
     return movies
